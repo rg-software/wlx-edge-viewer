ed6adc1dc651e0debe7368b89a6c5e4b77348b7e
Maxim Mozgovoy <mozgovoy@u-aizu.ac.jp>
2022-11-27 10:57:43 +0900

    do not "steal" other file types (to be refactored)

--- EdgeViewer/Processors/ProcessorInterface.h
+++ EdgeViewer/Processors/ProcessorInterface.h
@@ -10,6 +10,8 @@ public:
 	virtual bool Load(const fs::path& path) = 0;
 	virtual void OpenIn(ViewPtr webView) const = 0;
 	
+	// TODO: refactor it (code duplication)
+	static bool RegistryLoad(const fs::path& path);
 	static bool RegistryLoadAndOpen(const fs::path& path, ViewPtr webView);
 
 private:
