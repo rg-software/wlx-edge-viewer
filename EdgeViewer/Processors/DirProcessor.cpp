a93763499ea328c9c3f94fd9985a8491e692bf8d
Maxim Mozgovoy <mozgovoy@u-aizu.ac.jp>
2023-09-29 10:21:21 +0900

    small refactoring

--- EdgeViewer/Processors/DirProcessor.cpp
+++ EdgeViewer/Processors/DirProcessor.cpp
@@ -15,10 +15,8 @@ void DirProcessor::OpenIn(ViewPtr webView) const
 { 
 	mapDomains(webView, mPath.root_path());
 
-	std::string loader(readFile(assetsPath() / L"dirviewer" / L"loader.html"));
-	loader = std::regex_replace(loader, std::regex("__BASE_URL__"), urlPath(mPath.relative_path()));
-	
-	std::wstring wloader = to_utf16(loader);
+	std::wstring wloader(to_utf16(readFile(assetsPath() / L"dirviewer" / L"loader.html")));
+	wloader = std::regex_replace(wloader, std::wregex(L"__BASE_URL__"), urlPathW(mPath.relative_path()));
 	wloader = std::regex_replace(wloader, std::wregex(L"__BODY__"), genBody(mPath));
 
 	webView->NavigateToString(wloader.c_str());
@@ -28,7 +26,7 @@ std::wstring DirProcessor::genBody(const fs::path& path) const
 {
 	auto exts = gs_Ini()["Extensions"]["DirImageExt"];
 	auto mask = std::format(L".+\\.({})$", to_utf16(exts));
-
+	auto re = std::wregex(mask, std::regex_constants::icase);
 	std::wstringstream ss;
 
 	for (auto const& dir_entry : fs::directory_iterator(path))
@@ -36,7 +34,7 @@ std::wstring DirProcessor::genBody(const fs::path& path) const
 		{
 			auto fname = dir_entry.path().filename().wstring();
 
-			if (std::regex_match(fname, std::wregex(mask, std::regex_constants::icase)))
+			if (std::regex_match(fname, re))
 				ss << std::format(L"<a href=\"{0}\" class=\"thumbnail-viewer\">"
 								  L"<img class=\"thumbnail\" src=\"{0}\" alt=\"{0}\">", fname).c_str();
 		}
