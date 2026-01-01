3ba07ba075245d3651964b8798dae925c9a77295
Maxim Mozgovoy <mozgovoy@u-aizu.ac.jp>
2026-01-01 17:00:30 +0900

    dark/light mode for dirviewer; placeholder thumbnails for non-image files

--- EdgeViewer/Processors/DirProcessor.cpp
+++ EdgeViewer/Processors/DirProcessor.cpp
@@ -1,7 +1,6 @@
 #include "DirProcessor.h"
 #include <shlwapi.h>
 #include <wininet.h>
-#include <regex>
 #include <vector>
 #include <algorithm>
 //------------------------------------------------------------------------
@@ -21,21 +20,41 @@ bool DirProcessor::InitPath(const fs::path& path)
 //------------------------------------------------------------------------
 void DirProcessor::OpenIn(ViewPtr webView) const
 { 
+	const auto& dirIni = GlobalSettings().get("Directory");
+	const auto cssFile = gs_IsDarkMode ? dirIni.get("CSSDark") : dirIni.get("CSS");
+
 	mapDomains(webView, mPath.root_path());
 
 	std::wstring wloader(to_utf16(ReadFile(assetsPath() / L"dirviewer" / L"loader.html")));
 	wloader = std::regex_replace(wloader, std::wregex(L"__BASE_URL__"), urlPathW(mPath.relative_path()));
+	wloader = std::regex_replace(wloader, std::wregex(L"__CSS_NAME__"), to_utf16(cssFile));
+
+	wloader = std::regex_replace(wloader, std::wregex(L"__FIT_TO_SCREEN__"), to_utf16(dirIni.get("FitToScreen")));
+	wloader = std::regex_replace(wloader, std::wregex(L"__TRUNCATE_NAMES__"), to_utf16(dirIni.get("TruncateNames")));
+	wloader = std::regex_replace(wloader, std::wregex(L"__NAMES_UNDER_THUMBS__"), to_utf16(dirIni.get("NamesUnderThumbnails")));
+
 	wloader = std::regex_replace(wloader, std::wregex(L"__BODY__"), genBody(mPath));
 
 	webView->NavigateToString(wloader.c_str());
 }
 //------------------------------------------------------------------------
+std::wregex DirProcessor::extensionsToMaskRegex(const std::string& exts) const
+{
+	auto mask = std::format(L".+\\.({})$", to_utf16(exts));
+	return std::wregex(mask, std::regex_constants::icase);
+}
+//------------------------------------------------------------------------
 std::wstring DirProcessor::genBody(const fs::path& path) const
 {
-	auto exts = GlobalSettings()["Extensions"]["DirImageExt"];
-	auto mask = std::format(L".+\\.({})$", to_utf16(exts));
-	auto re = std::wregex(mask, std::regex_constants::icase);
+	const auto& dirIni = GlobalSettings().get("Directory");
+	const auto cssFile = gs_IsDarkMode ? dirIni.get("CSSDark") : dirIni.get("CSS");
+
+	auto imageExtRe = extensionsToMaskRegex(dirIni.get("DirImageExt"));
+	auto otherExtRe = extensionsToMaskRegex(dirIni.get("DirOtherExt"));
+
 	auto folderThumb = L"http://assets.example/dirviewer/folder.png";
+	auto fileThumb = L"http://assets.example/dirviewer/folder.png";
+
 	std::wstringstream ss;
 
 	std::vector<fs::directory_entry> entries;
@@ -63,10 +82,15 @@ std::wstring DirProcessor::genBody(const fs::path& path) const
 			ss << std::format(L"<div class=\"directory-viewer\" data-name=\"{0}\">" 
 							  L"<img class=\"thumbnail\" src=\"{1}\" alt=\"{0}\"></div>", fname, folderThumb).c_str();
 		}
-		else if (dir_entry.is_regular_file() && std::regex_match(fname, re))
+		else if (dir_entry.is_regular_file() && std::regex_match(fname, imageExtRe))
 		{
 			ss << std::format(L"<a href=\"{0}\" class=\"thumbnail-viewer\">"
 							  L"<img class=\"thumbnail\" src=\"{0}\" alt=\"{0}\"></a>", fname).c_str();
+		}
+		else if (dir_entry.is_regular_file() && std::regex_match(fname, otherExtRe))
+		{
+			ss << std::format(L"<div class=\"directory-viewer\" data-name=\"{0}\">"
+				L"<img class=\"thumbnail\" src=\"{1}\" alt=\"{0}\"></div>", fname, fileThumb).c_str();
 		}
 	}
 	
